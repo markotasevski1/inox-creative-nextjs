c4fabe6660df3e682e4765f63db8a0556d9e6d47
markotasevski1 <markotasevski123@yahoo.com>
2023-08-16 16:38:11 +0200

    Created cta section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import CtaSection from '@/components/cta-section/CtaSection'
-import DemoCtaSection from '@/components/cta-section/DemoCtaSection'
+import { DemoCtaSection } from '@/components/cta-section/DemoCtaSection'
 import FeaturedSection from '@/components/featured-section/FeaturedSection'
 import { MainHeroSection } from '@/components/hero-sections/MainHeroSection'
 import SolutionsShowcase from '@/components/solutions-showcase/SolutionsShowcase'
