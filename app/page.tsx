91aaaa0546cf580e8837af2691722acc480f075c
markotasevski1 <markotasevski123@yahoo.com>
2023-08-16 16:39:50 +0200

    Created all ui on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CtaSection from '@/components/cta-section/CtaSection'
 import { DemoCtaSection } from '@/components/cta-section/DemoCtaSection'
 import FeaturedSection from '@/components/featured-section/FeaturedSection'
+import Footer from '@/components/footer/Footer'
 import { MainHeroSection } from '@/components/hero-sections/MainHeroSection'
 import SolutionsShowcase from '@/components/solutions-showcase/SolutionsShowcase'
 
@@ -13,6 +14,7 @@ export default function Home() {
       <SolutionsShowcase />
       <FeaturedSection />
       <DemoCtaSection />
+      <Footer />
     </>
   )
 }
