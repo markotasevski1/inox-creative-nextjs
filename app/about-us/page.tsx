12b3cab5c6be75207059c1ba6f543aff89c65c0b
markotasevski1 <markotasevski123@yahoo.com>
2023-08-27 03:45:05 +0200

    Fixed error with fetching data

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
+async function getData() {
+  const res = await fetch('http://127.0.0.1:1337/api/machines')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-export default function page() {
-  return <div>about us</div>
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Page() {
+  const data = await getData()
+  console.log(data)
+  return <main></main>
 }
